39f6e84a123237f91eceecd38f0927f3afb24b1b
wangyannan <wangyannan@meituan.com>
2016-02-05 10:55:35 +0800

    Add the function to response wx

--- helloworld.py
+++ helloworld.py
@@ -19,17 +19,47 @@ from tornado import httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-
+import hashlib
 from tornado.options import define, options
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=80, help="run on the given port", type=int)
+
+
+def checkSignatureWX(signature = '',timestamp = '',nonce = '',token = ''):
+    tempList = [token,timestamp,nonce]
+    tempList2 = sorted(tempList)
+    str_ = tempList2[0] + tempList2[1] + tempList2[2]
+    if (str(hashlib.sha1(str_).hexdigest()) == signature)
+        return 1
+    else
+        return 0
+
+
+#def getParam(ServerRequest):
+    #uri = ServerRequest.uri
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
-	self.write("   ")
-	self.write(self.request.url)
+        uri = self.request.uri
+        param_list = uri.split('&')
+        token = 'nan_first_wx_token'
+        for i in range(len()):
+            pos = param_list[i].find('=') + 1
+            if (param_list[i].find('signature') > 0):
+                signature = param_list[i][pos:]
+            if (param_list[i].find('timestamp') > 0):
+                timestamp = param_list[i][pos:]
+            if (param_list[i].find('nonce') > 0):
+                nonce = param_list[i][pos:]
+            if (param_list[i].find('echostr') > 0):
+                echostr = param_list[i][pos:]
+
+        if (checkSignatureWX(signature,timestamp,nonce,token) == 1):
+            self.write(echostr)
+        else:
+            self.write("It is not from wx")
+
 
 
 def main():
